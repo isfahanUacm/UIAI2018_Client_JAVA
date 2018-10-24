7d60168db030bb95f17a0ce470a68081d8f6a00d
mdsinalpha <mdsinalpha@localhost.localdomain>
2018-10-24 11:44:50 +0330

    Trig method added to game!

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@
 public class Main{
     
     public static void main(String[] args){
-        Game game = new Game("127.0.0.1",9595);
+        Game game = new Game("127.0.0.1", 9595);
         try{
             game.trig("Team1", null);
         }catch(Exception e){
